c0af07906732ed8fb17bd2942f6b9fc0968e036f
PSB08 <Parkseongbin08@gmail.com>
2025-01-21 01:43:24 +0900

    Enum Setting
    Video1 / 5 : 30

--- Assets/00.Work/01.Scripts/Enums/PieceType.cs
+++ Assets/00.Work/01.Scripts/Enums/PieceType.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardLayout : MonoBehaviour
+public enum PieceType
 {
-
+    Pawn, Bishop, Knight, Rook, Queen, King
 }
