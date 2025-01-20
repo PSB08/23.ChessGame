f8872f4e3189344bac1d17131ad463327eb9f855
PSB08 <Parkseongbin08@gmail.com>
2025-01-21 01:40:07 +0900

    Script Setting

--- Assets/00.Work/01.Scripts/ChessGame/ChessGameController.cs
+++ Assets/00.Work/01.Scripts/ChessGame/ChessGameController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class ChessGameController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("게임시작");
+        
     }
 
     // Update is called once per frame
