c07e19a98caba251b3f73ebd54d97942e264c1f5
PSB08 <Parkseongbin08@gmail.com>
2025-01-22 23:43:06 +0900

    Bishop, Queen Move

--- Assets/00.Work/01.Scripts/ChessGame/Pieces/Queen.cs
+++ Assets/00.Work/01.Scripts/ChessGame/Pieces/Queen.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Queen : Piece
 {
+    private Vector2Int[] directions = new Vector2Int[]
+    {
+        new Vector2Int(1, 1),
+        new Vector2Int(1, -1),
+        new Vector2Int(-1, 1),
+        new Vector2Int(-1, -1),
+        Vector2Int.up,
+        Vector2Int.down,
+        Vector2Int.right,
+        Vector2Int.left,
+    };
     public override List<Vector2Int> SelectAvailableSquares()
     {
         availableMoves.Clear();
-        availableMoves.Add(occupiedSquare + new Vector2Int(0, 1));
+        float range = Board.BOARD_SIZE;
+        foreach (var direction in directions)
+        {
+            for (int i = 1; i < range; i++)
+            {
+                Vector2Int nextCoords = occupiedSquare + direction * i;
+                Piece piece = board.GetPieceOnSquare(nextCoords);
+                if (!board.CheckIfCoordinatedAreOnBoard(nextCoords))
+                {
+                    break;
+                }
+                if (piece == null)
+                {
+                    TryToAddMove(nextCoords);
+                }
+                else if (!piece.IsFromSameTeam(this))
+                {
+                    TryToAddMove(nextCoords);
+                    break;
+                }
+                else if (piece.IsFromSameTeam(this))
+                    break;
+            }
+        }
         return availableMoves;
     }
 
